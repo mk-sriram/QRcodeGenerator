ad916cc159f31b840998b6e4e803e7ba1e036abb
Sriram MK <mksriram@umich.edu>
2024-01-16 12:21:34 -0500

    added basic javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const shortBtn = document.getElementsByClassName('short-btn');
+
+shortBtn.addEventListener('click', qrCodeGenerator);
+
+function qrCodeGenerator(){
+    console.log('button activated')
+    const originalText = document.getElementById('originaltext');
+
+    console.log('past OriginalText')
+}
