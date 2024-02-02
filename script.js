9f75c55c4789064d2c74ea0907b718f6add0b32b
Sriram MK <mksriram@umich.edu>
2024-02-01 19:47:12 -0500

    addded button fucitnoality

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 
-const shortBtn = document.getElementsByClassName('short-btn');
+const shortBtn = document.querySelector('short-btn');
 
 shortBtn.addEventListener('click', qrCodeGenerator);
 
 function qrCodeGenerator(){
+    
     console.log('button activated')
     const originalText = document.getElementById('originaltext');
-
     console.log('past OriginalText')
 }
 
